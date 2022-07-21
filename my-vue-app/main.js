fe7e174a43c510c7bfb492ba291c354b0fac6ee8
Robert Simbotin <70038614+SmartBizCode2@users.noreply.github.com>
2022-07-21 11:22:42 +0200

    a

---
+++ my-vue-app/main.js
@@ -0,0 +1,10 @@
+window.getForumIcons = () => {
+  return [
+    {
+      id: 0,
+      src: "https://i.imgur.com/XqQZQ.png",
+      altText: "Default",
+      category: "One Piece",
+    },
+  ];
+};
