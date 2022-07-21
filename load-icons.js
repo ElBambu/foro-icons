00d29979e789b357f085db9203bd2972dacc54d7
Robert Simbotin <70038614+SmartBizCode2@users.noreply.github.com>
2022-07-21 11:47:21 +0200

    s

--- load-icons.js
+++ load-icons.js
@@ -1,3 +1,10 @@
 window.getForumIcons = () => {
-  return [{ id: 0, src: "https://i.imgur.com/XqQZQ.png", name: "Default" }];
+  return [
+    { id: 0, src: "https://i.imgur.com/XqQZQ.png", name: "Default" },
+    { id: 1, src: "https://i.imgur.com/XqQZQ.png", name: "Default" },
+    { id: 2, src: "https://i.imgur.com/XqQZQ.png", name: "Default" },
+    { id: 3, src: "https://i.imgur.com/XqQZQ.png", name: "Default" },
+    { id: 4, src: "https://i.imgur.com/XqQZQ.png", name: "Default" },
+    { id: 5, src: "https://i.imgur.com/XqQZQ.png", name: "Default" },
+  ];
 };
